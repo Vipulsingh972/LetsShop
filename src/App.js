4973b00dc096194d45bf11a248296586b20b257e
SHREYASH0747 <96493968+SHREYASH0747@users.noreply.github.com>
2025-03-26 12:12:42 +0530

    Resolved merge conflict

--- src/App.js
+++ src/App.js
@@ -1,37 +1,65 @@
 import React from "react";
-import Navbarr from "./Components/Navbarr";
-import "../src/Style/navbar.css";
-import "../src/Style/login.css";
-import "../src/Style/about.css";
-import "../src/Style/contact.css";
-import "../src/Style/home.css";
+// <<<<<<< HEAD
+// import Navbarr from "./Components/Navbarr";
+// import "../src/Style/navbar.css";
+// import "../src/Style/login.css";
+// import "../src/Style/about.css";
+// import "../src/Style/contact.css";
+// import "../src/Style/home.css";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// =======
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbarr from "./Components/Navbarr";
+// >>>>>>> f0b5cda ( signup done)
 import Home from "./Components/Home";
 import Shop from "./Components/Shop";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Login from "./Components/Login";
 import SignUp from "./Components/Signup";
+// <<<<<<< HEAD
 import ProductDetails from "./Components/ProductDetails";
 import Cart from "./Components/Cart";
 
+// const App = () => {
+//   return (
+//     <Router>
+//       <>
+//         <Navbarr />
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/shop" element={<Shop />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/contact" element={<Contact />} />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/signup" element={<SignUp />} />
+//           <Route path="/product-details" element={<ProductDetails />} />
+//           <Route path="/cart" element={<Cart />} />
+//         </Routes>
+//       </>
+//     </Router>
+// =======
+// import Cart from "./Components/Cart";
+
 const App = () => {
   return (
-    <Router>
-      <>
-        <Navbarr />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/product-details" element={<ProductDetails />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </>
-    </Router>
+    
+      <Router>
+        <>
+          <Navbarr />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            {/* <Route path="/cart" element={<Cart />} /> */}
+          </Routes>
+        </>
+      </Router>
+   
+// >>>>>>> f0b5cda ( signup done)
   );
 };
 
