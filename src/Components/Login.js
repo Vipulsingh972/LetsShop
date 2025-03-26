a648b841a792dd2d58ebc581c90738ee13195e62
Your Name <vs902388@gmail.com>
2025-03-26 15:13:26 +0530

    Login api is done

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,8 +2,36 @@ import React from "react";
 import "../Style/login.css";
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
+import ApiService from "../service/ApiService";
+import { useState } from "react";
 
 const Login = () => {
+  const [userdata, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    setUserData({ ...userdata, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await ApiService.login(userdata); // Use signup method instead of register
+      alert("LogIn successful!"); // Alert on success
+      setUserData({
+        email: "",
+        password: "",
+      });
+    } catch (error) {
+      setError(error.message || "An error occurred during LogIn."); // Show error message
+    }
+  };
+
   return (
     <>
       <div className="login-container">
@@ -16,9 +44,24 @@ const Login = () => {
             get access to your dashboard.
           </p>
 
-          <form>
-            <input type="text" placeholder="Username or Email *" required />
-            <input type="password" placeholder="Password *" required />
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Email *"
+              name="email"
+              value={userdata.email}
+              onChange={handleChange}
+              required
+            />
+
+            <input
+              type="password"
+              placeholder="Password *"
+              name="password"
+              value={userdata.password}
+              onChange={handleChange}
+              required
+            />
 
             <div className="options">
               <label>
